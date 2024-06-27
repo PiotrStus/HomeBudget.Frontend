dd46e6a7294bacf2cea35179161a2954039eebf4
Piotr Stus <pm2024love@gmail.com>
2024-06-27 10:11:54 +0200

    Change theme button added.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,36 @@
 <template>
 	<v-app>
-		<v-app-bar color ='brand'>
-			<v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+		<v-app-bar color="brand">
+			<v-app-bar-nav-icon
+				v-if="mobile"
+				@click="drawer = !drawer"
+			></v-app-bar-nav-icon>
 
 			<v-app-bar-title>Page Monitor</v-app-bar-title>
+
+			<v-spacer></v-spacer>
+			<VBtn
+				icon="mdi-theme-light-dark"
+				title="Przełącz motyw"
+				@click="toggleTheme"
+			></VBtn>
 		</v-app-bar>
 
 		<!-- MENU -->
-		<v-navigation-drawer :order="mobile ? -1 : 0"   v-model="drawer">
+		<v-navigation-drawer :order="mobile ? -1 : 0" v-model="drawer">
 			<VList>
-				<VListItem v-for="item in menuItems" :key="item.name" :title="item.name" :prepend-icon="item.icon" :to="item.url"></VListItem>
-
+				<VListItem
+					v-for="item in menuItems"
+					:key="item.name"
+					:title="item.name"
+					:prepend-icon="item.icon"
+					:to="item.url"
+				></VListItem>
 			</VList>
 		</v-navigation-drawer>
 
-
-
 		<v-main>
-			<div class ="pa-4">
+			<div class="pa-4">
 				<NuxtPage />
 			</div>
 		</v-main>
@@ -26,24 +39,45 @@
 
 <script setup>
 // to jest wlasnie composables
-import { useDisplay } from 'vuetify'
+import { useDisplay } from "vuetify";
+import { useTheme } from "vuetify";
+import { useStorage } from "@vueuse/core";
 
-const { mobile } = useDisplay()
+// tworzymy obiekt, ktory uzywa tego composable
+const theme = useTheme();
+
+const { mobile } = useDisplay();
+
+const currentTheme = useStorage("currentTheme", "light");
 
 const drawer = ref(null);
 
 const menuItems = [
 	{
-		name: 'Strona główna',
-		icon: 'mdi-home',
-		url: '/'
+		name: "Strona główna",
+		icon: "mdi-home",
+		url: "/",
 	},
 	{
-		name: 'Monitorowane strony',
-		icon: 'mdi-web-check',
-		url: '/urls'
+		name: "Monitorowane strony",
+		icon: "mdi-web-check",
+		url: "/urls",
 	},
 ];
 
+function toggleTheme() {
+	// zmienne lokalne deklaruje się za pomocą let
+	let newTheme = theme.global.current.value.dark ? "light" : "dark";
+
+	theme.global.name.value = newTheme;
+
+	currentTheme.value = newTheme;
+}
+
+// w moemncie kiedy komponent jest podlaczany do drzewa i wyswietlany
+onMounted( () => {
+	// czyli przypisujemy wartosc czytana z local storage
+	theme.global.name.value = currentTheme.value;
+});
 
 </script>
